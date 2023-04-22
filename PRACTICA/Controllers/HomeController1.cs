16832bb5022e7d221638bd0d657083aaacb70422
CarlosD <carlos.chinchilla1@catolica.edu.sv>
2023-04-21 22:22:49 -0600

    Razor

---
+++ PRACTICA/Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRACTICA.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
